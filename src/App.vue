8aa614e9fba9a92761df78bc300336269f225731
Hongbusi <coderhbs@gmail.com>
2022-10-18 10:22:28 +0800

    chore: update readme

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <script setup lang="ts">
 useHead({
-  title: 'starter-vue',
-  meta: [
-    { name: 'description', content: 'A template to quickly create a vue project.' }
-  ]
+  title: 'print-receipt'
 })
 </script>
 
