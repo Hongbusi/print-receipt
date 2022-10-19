1d5b7e7c793c816d4510de154bc80ca73f7ede89
Hongbusi <coderhbs@gmail.com>
2022-10-19 10:55:36 +0800

    chore: test

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,65 +1,32 @@
 <script setup lang="ts">
-// import domToImage from 'dom-to-image'
-// import html2canvas from 'html2canvas'
-// import printJS from 'print-js'
 import Print from './print'
 
-const print = new Print()
-
-print.addPrintText('冠山示范型居家养老服务中心消费结账单')
-print.addPrintText('冠山示范型居家养老服务中心消费结账单')
-print.addPrintBlankLine()
-print.addPrintText('冠山示范型居家养老服务中心消费结账单')
-print.addPrintText('冠山示范型居家养老服务中心消费结账单')
+const print = new Print({
+  header: '冠山示范型居家养老服务中心消费结账单',
+  nodes: [
+    { type: 'text', content: '这是一个测试' },
+    { type: 'blank-line' },
+    {
+      type: 'list',
+      title: '这是一个测试列表',
+      content: [
+        ['这是第一个', '这是第二个', '这是第三个'],
+        ['这是第一个', '这是第二个', '这是第三个']
+      ]
+    },
+    { type: 'text', content: '这是一个测试' },
+    { type: 'text', content: '这是一个测试' },
+    { type: 'text', content: '这是一个测试' }
+  ]
+})
 
 function handleClickPrint() {
-  print.print()
+  print.toPrint()
 }
-
-// async function print() {
-//   // const dataUrl = await domToImage.toPng(document.querySelector('#print-container')!)
-
-//   printJS({
-//     printable: 'print-container',
-//     type: 'html',
-//     style: 'div { word-break: break-all; font-size: 12px; zoom: 0.95; font-weight: 400; } ul { padding: 0; margin: 0; list-style: none; display: flex; } ul li {width: 30%; display: inline-block;} .blank-line {margin-top:6px;}',
-//     header: '冠山示范型居家养老服务中心消费结账单',
-//     headerStyle: 'font-size: 12px;',
-//     scanStyles: false
-//   })
-
-//   // html2canvas(document.querySelector('#print-container')!, {
-//   //   scale: 50
-//   // }).then((canvas) => {
-//   //   const dataUrl = canvas.toDataURL('image/png')
-//   //   printJS({
-//   //     printable: dataUrl,
-//   //     type: 'image',
-//   //     imageStyle: 'width: 100%;'
-//   //   })
-//   // })
-// }
 </script>
 
 <template>
-  <div class="p-5 mx-auto my-20 w-100 border text-center">
-    <!-- <div id="print-container">
-      <div>姓名：张三</div>
-      <div>单号：123442342421442单号：123442342421442</div>
-      <div>收营员：李四</div>
-      <div class="blank-line" />
-      <ul>
-        <li>这是菜名</li>
-        <li>价格</li>
-        <li>数量</li>
-      </ul>
-      <div class="blank-line" />
-      <div>结账时间：2022-10-20</div>
-      <div>消费金额：67元</div>
-      <div> 账户余额：1000元</div>
-    </div> -->
-
-    <hr class="my-6">
+  <div class="p-5 py-20 text-center">
     <button class="px-2 py-1 border" @click="handleClickPrint">
       Click Print
     </button>
--- src/pages/print.js
+++ src/pages/print.js
@@ -1,32 +1,92 @@
 import printJS from 'print-js'
 
 export default class Print {
-  constructor() {
-    this.elStr = ''
+  code = ''
+
+  constructor(options) {
+    this.options = options
+
+    this.generate()
   }
 
-  addPrintText(text) {
-    this.elStr += `<div>${text}</div>`
+  generate() {
+    this.push('<div id="print-container">')
+
+    this.options.nodes.forEach((node) => {
+      switch (node.type) {
+        case 'text':
+          this.genText(node.content)
+          break
+
+        case 'list':
+          this.genList(node.title, node.content)
+          break
+
+        case 'blank-line':
+          this.genBlankLine()
+          break
+
+        default:
+          break
+      }
+    })
+
+    this.push('</div>')
   }
 
-  addPrintBlankLine() {
-    this.elStr += '<div class="blank-line" />'
+  push(source) {
+    this.code += source
   }
 
-  print() {
-    const divEl = document.createElement('div')
-    divEl.id = 'print-container'
-    divEl.innerHTML = this.elStr
+  genText(text) {
+    this.push(`<div>${text}</div>`)
+  }
 
-    document.body.appendChild(divEl)
+  genList(title, content) {
+    this.genText(title)
+    this.push('<ul>')
 
+    content.forEach((i) => {
+      i.forEach((j) => {
+        this.push(`<li>${j}</li>`)
+      })
+    })
+
+    this.push('</ul>')
+  }
+
+  genBlankLine() {
+    this.push('<div class="blank-line" />')
+  }
+
+  insert() {
+    const div = document.createElement('div')
+    div.style = 'position: fixed; bottom: 0; left: 100%;'
+    div.innerHTML = this.code
+    return document.body.appendChild(div)
+  }
+
+  remove(node) {
+    const parent = node.parentNode
+    if (parent)
+      parent.removeChild(node)
+  }
+
+  toPrint() {
+    const node = this.insert()
+
+    // eslint-disable-next-line @typescript-eslint/no-this-alias
+    const _this = this
     printJS({
       printable: 'print-container',
       type: 'html',
-      style: 'div { word-break: break-all; font-size: 12px; zoom: 0.95; font-weight: 400; } ul { padding: 0; margin: 0; list-style: none; display: flex; } ul li {width: 30%; display: inline-block;} .blank-line {margin-top:6px;}',
-      header: '冠山示范型居家养老服务中心消费结账单',
+      style: 'div { word-break: break-all; font-size: 12px; zoom: 0.95; font-weight: 400; } ul { padding: 0; margin: 0; list-style: none; display: flex; } ul li { width: 33%; display: inline-block; } .blank-line { margin-top: 6px; }',
+      header: this.options.header,
       headerStyle: 'font-size: 12px;',
-      scanStyles: false
+      scanStyles: false,
+      onLoadingEnd() {
+        _this.remove(node)
+      }
     })
   }
 }
